0ea191ed09b6bb9ef5b6f35ac03d74c1092e8905
paulacaicedo <paula.caicedo.2019@upb.edu.co>
2019-03-01 12:57:05 -0500

    ejercicio resulto

--- mayor.py
+++ mayor.py
@@ -1,12 +1,22 @@
 
 
-print("Programa de numero mayor")
+positivos=0
+negativos=0
+ceros=0
 
-x=int(input("Intrduzca un valor entero: "))
+i=0
+for i in range(0,11):
+    a=int(input("Introduzca un valor: "))
+    if a==0:
+        print(a)
+        ceros=ceros+1
+    elif a>0:
+        print(a)
+        positivos=positivos+1
+    else:
+        print(a)
+        negativos=negativos+1
 
-if x>=0:
-    print("Valor mayor")
-else:
-    print("Valor negativo, Menor a cero")
-
-print("Programa Finalizado ")
+print("los positivos son: ",positivos)
+print("los positivos son: ",negativos)
+print("los positivos son: ",ceros)
