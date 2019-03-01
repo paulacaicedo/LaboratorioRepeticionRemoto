7cc297fe5cc0040a13d0a2d4ca0885498582964b
paulacaicedo <paula.caicedo.2019@upb.edu.co>
2019-03-01 13:05:22 -0500

    ejercicio dos resuelto

--- pares.py
+++ pares.py
@@ -2,7 +2,10 @@
 
 x=int(input("Intrduzca un valor entero: "))
 
-if x%2==0:
-    print("Numero par")
-else:
-    print("Numero impar")
+while (x%2==0):
+    x = int(input("Intrduzca un valor entero: "))
+    if (x%2!=0):
+         break
+
+
+
